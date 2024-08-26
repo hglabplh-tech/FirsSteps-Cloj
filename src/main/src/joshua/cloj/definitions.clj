3ce0e6a03904869173edc2940e32d13665caacf8
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-08-26 13:44:40 +0100

    first step

--- src/main/src/joshua/cloj/definitions.clj
+++ src/main/src/joshua/cloj/definitions.clj
@@ -1,1 +1,34 @@
-(ns joshua.cloj.definitions)
+(ns joshua.cloj.definitions
+  (:refer-clojure :exclude [def defn fn])
+  (:require [active.data.realm :as realm]
+            [schema.core :as schema]
+            [active.data.realm.attach :refer :all]
+           ))
+
+
+(schema/set-fn-validation! (boolean 1))
+
+(defn i-am-a-fake-fun :- realm/boolean
+      [msg :- realm/string a :- realm/number b :- realm/number]
+      (let [res (* a b)]
+        (println msg res)
+        (boolean res)))
+
+(defn i-return-somewhat  :- realm/boolean
+      [msg :- realm/string a :- realm/number b :- realm/number]
+      (let [res (* a b)]
+        (println msg res)
+        (boolean res)
+        "blah"))
+
+(i-am-a-fake-fun "huhu" 8 9)
+
+;; error ?? whats wrong ?
+;;(i-am-a-fake-fun 10 8 9)
+
+;;whats wrong here
+;;(i-return-somewhat "huhu" 8 9)
+
+;; Make a definition of a calculator for * + / - by do curring
+;; hint + / - .... are normal functions - material can be found in deinprogramm.de
+;; do it using the typed version of functions like above
